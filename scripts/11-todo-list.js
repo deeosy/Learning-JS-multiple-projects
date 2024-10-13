2b0f685d61c3b135645bac9afab2bdd69d8e35d8
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2024-10-13 16:23:54 +0000

    updated todo List

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -6,20 +6,46 @@ function renderTodoList() {
   let todoListHTML = '';
   
   for(let i=0; i<todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    // const name = todoObject.name; 
+    // const dueDate = todoObject.dueDate;
+    // const dueTime = todoObject.dueTime;
+
+    // short hand property (destructurting) for all the code above will be the line of code on line 26
+    const {name, dueDate, dueTime} = todoObject;
+
+    const html = `
+      <div>${name}</div> 
+      <div>${dueDate}</div>
+      <div>${dueTime}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="remove-button">Remove</button>
+    `;
     todoListHTML += html;
-    console.log(todoListHTML)
   }
-  console.log(todoListHTML);
   document.querySelector('.js-todoList').innerHTML = todoListHTML;
 }
 
 function addTodo() {
   const inputElement = document.querySelector(".js-todoInput");
-  const todoName = inputElement.value;
-  todoList.push(todoName);
-  console.log(todoList);
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector('.js-todoDate');
+  const dueDate = dateInputElement.value;
+  const timeInputElement = document.querySelector('.js-todoTime');
+  const dueTime = timeInputElement.value;
+
+  todoList.push(
+    {
+      // name: name,
+      // dueDate: dueDate,
+      // dueTime: dueTime,
+      name,
+      dueDate,
+      dueTime,
+    }
+  );
   inputElement.value = '';
   renderTodoList();
 }
